4c71e63b3a418664ceff24c0bb3d42ab22e6bb76
viktoriazaharova <zaharova181114@gmail.com>
2020-11-24 20:51:36 +0200

    add block

--- app/js/common.js
+++ app/js/common.js
@@ -5,7 +5,6 @@ $(window).on('load resize', function() {
         $('.home-menu:not(.slick-initialized)').slick({
             dots: false,
             // infinite: false,
-            speed: 100,
             slidesToShow: 1,
             centerMode: true,
             variableWidth: true,
@@ -21,8 +20,6 @@ $(window).on('load resize', function() {
     else if ((checkWidth < 650) && (checkWidth > 0)) {
         $('.home-menu:not(.slick-initialized)').slick({
             dots: false,
-            // infinite: false,
-            speed: 100,
             slidesToShow: 1,
             centerMode: true,
             variableWidth: true,
@@ -39,10 +36,18 @@ $(window).on('load resize', function() {
         $(".home-menu.slick-initialized").slick("unslick");
     }
 
-    // if (checkWidth < 700) {
-    //     $(".home-menu.slick-initialized").slick("unslick");
-    // }
-
 
 });
 // slick active
+
+$('.main-home__slider').slick({
+    slidesToShow: 1,
+    arrows: true,
+    nextArrow: '<button type="button" class="slick-next"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '    <path d="M0.999999 1L7 7L1 13" stroke="#333333" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+        '</svg>\n</button>',
+    prevArrow: '<button type="button" class="slick-prev"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '    <path d="M7 13L1 7L7 1" stroke="#333333" stroke-linecap="round" stroke-linejoin="round"/>\n' +
+        '</svg>\n</button>',
+    fade: true
+});
