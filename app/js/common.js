305d5a763a128642639b1aaec9254173cdd33336
viktoriazaharova <zaharova181114@gmail.com>
2020-11-25 12:48:11 +0200

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -43,6 +43,7 @@ $(window).on('load resize', function() {
             slidesToShow: 1,
             centerMode: true,
             variableWidth: true,
+            focusOnSelect: true,
             arrows: true,
             asNavFor: '.tab_container',
             nextArrow: '<button type="button" class="slick-next"><svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
